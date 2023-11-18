dced3788118d475ceaf9a97e2076c440683dbde4
Shaqs0 <hhgfhhhggb@gmail.com>
2023-11-18 17:13:58 +0300

    .

--- PZ_5/PZ_5_2.py
+++ PZ_5/PZ_5_2.py
@@ -7,6 +7,11 @@ def InvertDigits(K):
     return reversed_str[::-1]  
 
 try:
-    print(InvertDigits(int(input('Введите целое положительное число: '))))
+    print(InvertDigits(9406))
+    print(InvertDigits(3474))
+    print(InvertDigits(1112))
+    print(InvertDigits(4326))
+    print(InvertDigits(7453))
+
 except:
     print('Нужно ввести целое положительное число!')
